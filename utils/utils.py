e815ca862233da8e5df3118bf7e40c89eaa136e1
G_bandeira <bandeira156@gmail.com>
2025-01-13 15:29:02 -0300

    updates

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,13 @@
-import pandas as pd
-
 def takePandas(self, n=5):
-    rows = self.take(n)
-    df = pd.DataFrame(rows, columns=self.columns)
+    import pandas as pd
+
+    df_rows = self.take(n)
+    df = pd.DataFrame(df_rows, columns=self.columns)
+
     return df
 
 from pyspark.sql import DataFrame
 DataFrame.takePandas = takePandas
-
-def save_intermediary_step(df, path,mode="overwrite"):
-        df.write.parquet(path).mode(mode)
 
 def createSparkSesion():
     from pyspark.sql import SparkSession
@@ -21,3 +19,26 @@ def createSparkSesion():
     print(spark)
     
     return spark
+
+def save_intermediary_step(df, path,mode="overwrite"):
+        df.write.parquet(path).mode(mode)
+
+class Dog:
+    """ Blueprint of a dog """
+    # class variable shared by all instances
+    #just to remender about classes use case , out of practice
+    species = ["canis lupus"]
+    def __init__(self, name, color):
+        self.name = name
+        self.state = "sleeping"
+        self.color = color
+    def command(self, x):
+        if x == self.name:
+            self.bark(2)
+        elif x == "sit":
+            self.state = "sit"
+        else:
+            self.state = "wag tail"
+    def bark(self, freq):
+        for i in range(freq):
+            print("[" + self.name+ "]: Woof!")
