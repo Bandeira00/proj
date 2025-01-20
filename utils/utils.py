3bd07cc117103cef6e226641fa97da7024140439
G_bandeira <bandeira156@gmail.com>
2025-01-20 20:26:12 -0300

    new analysis notebook

--- utils/utils.py
+++ utils/utils.py
@@ -22,23 +22,3 @@ def createSparkSesion():
 
 def save_intermediary_step(df, path,mode="overwrite"):
         df.write.parquet(path).mode(mode)
-
-class Dog:
-    """ Blueprint of a dog """
-    # class variable shared by all instances
-    #just to remender about classes use case , out of practice
-    species = ["canis lupus"]
-    def __init__(self, name, color):
-        self.name = name
-        self.state = "sleeping"
-        self.color = color
-    def command(self, x):
-        if x == self.name:
-            self.bark(2)
-        elif x == "sit":
-            self.state = "sit"
-        else:
-            self.state = "wag tail"
-    def bark(self, freq):
-        for i in range(freq):
-            print("[" + self.name+ "]: Woof!")
